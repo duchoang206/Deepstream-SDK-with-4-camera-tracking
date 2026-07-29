14eeb2ace888086c1cb48e4794c431b6d476ef23
rtc <buimanhcan@rtc.tail84be74.ts.net>
2026-07-29 14:03:17 +0700

    Add WebRTC streaming via MediaMTX and Web Dashboard

--- DeepStream-RTSP-Demo/run_demo.sh
+++ DeepStream-RTSP-Demo/run_demo.sh
@@ -5,15 +5,17 @@ xhost +local:docker
 
 # Build the docker image
 echo "Building Docker image ds_rtsp_demo..."
-sudo docker build -t ds_rtsp_demo .
+docker build -t ds_rtsp_demo .
 
 # Run the docker container
 echo "Running Docker container ds_rtsp_demo..."
-sudo docker run -it --rm \
+docker run -it --rm \
   --gpus all \
   --net=host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY \
-  -v $(pwd):/app \
+  -e DISPLAY="$DISPLAY" \
+  -v "$(pwd)":/app \
   -w /app \
   ds_rtsp_demo
+
+
